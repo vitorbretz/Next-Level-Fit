10f58190b951c750427d49a648535e7321a53bfd
vitorbretz <vitorbretz28@gmail.com>
2025-02-05 22:55:47 -0300

    slide sobre
    feito seleção do slide da pagina sobre

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const links = document.querySelectorAll(".header-menu a");
+const galeria = document.querySelectorAll('.about-img img');
+const galeriaContainer = document.querySelector('.about-img');
+
+function trocarImagem(event){
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width:1000px)').matches;
+    if(media){
+        galeriaContainer.prepend(img);
+    }
+    
+    
+}
+
+function eventosGaleria(img){
+    img.addEventListener('click', trocarImagem);
+}
 
 function ativarLink (links){
     const url = location.href;
@@ -9,3 +25,5 @@ function ativarLink (links){
     }
 }
 links.forEach(ativarLink)
+
+galeria.forEach(eventosGaleria)
